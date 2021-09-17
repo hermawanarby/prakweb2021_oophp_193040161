5ce488180e696476e119ce0b8c62226271f7084d
hermawanarby <hermawanbaru4@gmail.com>
2021-09-17 22:38:39 +0700

    menambahkan object produk4 berkategori game dan menampilkannya dengan method getLabel

--- produk.php
+++ produk.php
@@ -26,4 +26,12 @@ $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
 
-echo $produk3->getLabel();
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
