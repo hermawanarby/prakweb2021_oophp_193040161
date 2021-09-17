703961b1d4b7bde6d1639d485dba2fd6079c3bd1
hermawanarby <hermawanbaru4@gmail.com>
2021-09-17 22:10:00 +0700

    menambahkan property pada class produk

--- produk.php
+++ produk.php
@@ -1,11 +1,17 @@
 <?php 
 
-// Jualan Produk
-// Komik
-// Game
 class Produk {
-
+  public $judul = "judul",
+         $penulis = "penulis",
+         $penerbit = "penerbit",
+         $harga = 0;
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2);
