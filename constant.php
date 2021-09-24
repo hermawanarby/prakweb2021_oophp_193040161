8b02d811e50c731b5be82d7e7adbff1a3ec1097e
hermawanarby <hermawanbaru4@gmail.com>
2021-09-24 15:02:19 +0700

    menyelesaikan video tentang constant di oophp

---
+++ constant.php
@@ -0,0 +1,27 @@
+<?php
+
+// define('NAMA', 'Hermawan Arby');
+// echo NAMA;
+
+// echo '<br>';
+
+// const UMUR = 19;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'Hermawan';
+// }
+// echo Coba::NAMA;
+
+// echo __FILE__;
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+// echo coba();
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+$obj = new Coba;
+echo $obj->kelas;
